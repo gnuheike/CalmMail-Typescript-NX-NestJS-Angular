a932a32e4e98252d6bca39d9a2bc9450e9972e3e
gnuheike <gnuheike@gmail.com>
2025-06-27 14:47:54 -0600

    fix(jest): correct coverage directory path in in-memory adapter config

--- libs/backend/use-case-in-memory-adapter/jest.config.ts
+++ libs/backend/use-case-in-memory-adapter/jest.config.ts
@@ -6,5 +6,5 @@ export default {
         '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
     },
     moduleFileExtensions: ['ts', 'js', 'html'],
-    coverageDirectory: '../../coverage/backend/backend-use-case-mock-adapter',
+    coverageDirectory: '../../../coverage/backend/backend-use-case-mock-adapter',
 };
